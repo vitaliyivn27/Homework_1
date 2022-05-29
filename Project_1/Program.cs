4fe34120dab87eac3d66f2e2c63040dafd9fad90
vitaliyivn27 <105597233+vitaliyivn27@users.noreply.github.com>
2022-05-29 18:44:23 +0300

    Add project files.

---
+++ Project_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Sollution_1.Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select number of the day:");
+            var selectedDay = Console.ReadLine();
+            int a = 0;
+            if (int.TryParse(selectedDay, out a))
+            {
+                Console.WriteLine(IsDay(a));
+            }
+            else
+            {
+                Console.WriteLine($"wrong value");
+            }
+
+        }
+
+        static string IsDay(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return ($"The day is Monday");
+
+                case 2:
+                    return ($"The day is Tuesday");
+
+                case 3:
+                    return ($"The day is Wednesday");
+
+                case 4:
+                    return ($"The day is Thursday");
+
+                case 5:
+                    return ($"The day is Friday");
+
+                case 6:
+                    return ($"The day is Saturday");
+
+                case 7:
+                    return ($"The day is Sunday");
+
+                default:
+                    return ("Wrong value");
+
+            }
+        }
+
+    }
+}
---
+++ Project_2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sollution_1.Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number for show multiplication table:");
+            var userInput = Console.ReadLine();
+            var a = 0;
+            if (int.TryParse(userInput, out a))
+            {
+                for (var i = 1; i < 11; i++)
+                {
+                    Console.WriteLine(Table(a, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"wrong value");
+            }
+        }
+        static string Table(int number, int i)
+        {
+            return ($"{number}*{i}={number * i}");
+        }
+    }
+}
